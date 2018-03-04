99ed454cf9781f2ca58d7d6d988207bdaae41da2
QiXu <qi.xu@engie.com>
2018-03-05 00:50:56 +0100

    stacking + prediction

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-import sys
+from sklearn.model_selection import KFold
+import numpy as np
 
 
 class WarrantyToFloat(TransformerMixin, BaseEstimator):
@@ -26,3 +27,15 @@ class FillByMax(TransformerMixin, BaseEstimator):
         df = input_df.copy()
         df = df.fillna(self.max_float)
         return df
+
+
+def one_folder_out(df_train_prepro, target, regressor, cv_number, random_seed):
+    prediction = np.array(np.repeat(np.nan, df_train_prepro.shape[0]))
+    kf = KFold(n_splits=cv_number, shuffle=False, random_state=random_seed)
+    for train_index, test_index in kf.split(df_train_prepro):
+        x_train, x_test = df_train_prepro.iloc[train_index, :], df_train_prepro.iloc[test_index, :]
+        y_train, y_test = target[train_index], target[test_index]
+        clf_k = regressor
+        clf_k.fit(x_train, y_train)
+        prediction[test_index] = clf_k.predict(x_test)
+    return prediction
