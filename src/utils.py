ca04a129a354ae467e05e2d9209b44e1f11d8fa9
QiXu <qi.xu@engie.com>
2018-03-05 22:49:01 +0100

    submissions and refactor

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 class WarrantyToFloat(TransformerMixin, BaseEstimator):
+    """change the warranty from string to int"""
     def __init__(self):
         pass
 
@@ -17,6 +18,7 @@ class WarrantyToFloat(TransformerMixin, BaseEstimator):
 
 
 class FillByMax(TransformerMixin, BaseEstimator):
+    """fill missing by a given max float """
     def __init__(self, max_float=(2**63-1)):
         self.max_float = max_float
 
@@ -30,6 +32,17 @@ class FillByMax(TransformerMixin, BaseEstimator):
 
 
 def one_folder_out(df_train_prepro, target, regressor, cv_number, random_seed):
+    """
+    leave one folder out for fitting and predicting for the out folder
+    loop on all the folder to make a "out of bag" prediction for all data set
+    for a given regressor
+    :param df_train_prepro : preprocessed trainning data(X)
+    :param target : target(Y)
+    :param regressor : a trained regressor
+    :param cv_number : number of cross validation folder
+    :param random_seed : random state number
+    :return: prediction : prediction
+    """
     prediction = np.array(np.repeat(np.nan, df_train_prepro.shape[0]))
     kf = KFold(n_splits=cv_number, shuffle=False, random_state=random_seed)
     for train_index, test_index in kf.split(df_train_prepro):
@@ -39,3 +52,18 @@ def one_folder_out(df_train_prepro, target, regressor, cv_number, random_seed):
         clf_k.fit(x_train, y_train)
         prediction[test_index] = clf_k.predict(x_test)
     return prediction
+
+
+def make_submission(df_test_prepro, submission_origine, clf, submission_name):
+    """
+    predicting and write csv file
+    :param df_test_prepro : preprocessed test data
+    :param submission_origine : empty data frame to be filled by prediction
+    :param clf : a trained regressor
+    :param submission_name : file name of submission
+    """
+    # prediction for log(y+1)
+    submission = submission_origine.loc[:, ["id"]]
+    # back to attractiveness
+    submission["attractiveness"] = np.e**(clf.predict(df_test_prepro)) - 1
+    submission.to_csv('../submissions/'+submission_name+'.csv', sep=';', index=False)
